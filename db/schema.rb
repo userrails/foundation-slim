f80a0e14ae36748970e805d99b2ffcf5f8628745
rails <rails@rails>
2014-11-28 15:21:18 +0530

    added the contact model to create the contact form with slim

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141128074431) do
+ActiveRecord::Schema.define(version: 20141128095028) do
+
+  create_table "contacts", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "message"
+    t.text     "address"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
